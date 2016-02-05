9e94cb189632da6aab8875532cfc4e3e0b451911
charles daniel <charles.daniel@relovate.com>
2016-02-05 13:34:10 -0500

    added date, sort reverse, choose column

--- js/controllers/filterCtl.js
+++ js/controllers/filterCtl.js
@@ -1,22 +1,39 @@
-myApp.controller('filterController', ['$scope', 'emptyToEndFilter','$log',function($scope,emptyToEndFilter,$log) {
+myApp.controller('filterController', ['$scope', 'emptyToEndFilter','orderByFilter','$log',function($scope,emptyToEndFilter,orderByFilter,$log) {
 	this.greeting = 'Hola its greeting as now!';
 	this.array = [
-	              {name: ''},
-	              {name: ''},
-	              {name: ''},
-	              {name: 'Tobias'},
-	              {name: 'Jeff'},
-	              {name: 'Brian'},
-	              {name: 'Igor'},
-	              {name: 'James'},
-	              {name: 'Brad'}
+	              {name: '',date: new Date("2015-03-25T12:00:01")},
+	              {name: '',date: new Date("2016-03-25T12:00:01")},
+	              {name: '',date: new Date("2017-03-25T12:00:01")},
+	              {name: 'Tobias',date: new Date("2016-02-25T12:00:01")},
+	              {name: 'Jeff',date: new Date("2015-03-25T23:00:01")},
+	              {name: 'Brian',date: new Date("2015-03-25T16:00:01")},
+	              {name: 'Igor',date: new Date("2015-03-25T15:00:01")},
+	              {name: 'James',date: new Date("2015-03-25T13:00:01")},
+	              {name: 'Brad',date: new Date("2015-03-25T17:00:01")}
 	              
-	            ];
-	this.filteredArray = emptyToEndFilter(this.array, 'name');
+	 
+	              ];
+	this.rev=false;
+	this.sortcol='name';
+	this.filteredArray =[];
+	this.revsort=function () {
+		this.rev=!this.rev;
+		//alert(this.rev);
+		this.filteredArray = emptyToEndFilter(orderByFilter(this.array, this.sortcol,this.rev),'name');
+	}
+	
+	this.sortby=function () {		
+		this.filteredArray = emptyToEndFilter(orderByFilter(this.array, this.sortcol,this.rev),'name');
+	}
+	
+	
 	
 	 var  present = this.array.filter(function (item) {		
          return item['name'];
      });
 	 $log.debug(present);	
+	 
+	 
+	 
 	
 }]);	

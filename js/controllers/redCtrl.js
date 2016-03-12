7b24b7b0258197e51a3d4ff0f3516808bb3206b4
charles <charles.daniel@relovate.com>
2016-03-11 19:28:42 -0500

    filterFilter

--- js/controllers/redCtrl.js
+++ js/controllers/redCtrl.js
@@ -1,14 +1,34 @@
-myApp.controller('redCtrl', ['$scope', function($scope) {
+myApp.controller('redCtrl', ['$scope','$log','filterFilter', function($scope,$log,filterFilter) {
 	 $scope.format = 'M/d/yy h:mm:ss a';
 	 $scope.ch={};
 	 $scope.ch.name="chARLES";
 	 $scope.ch.addr="55 road W";
 	 
+	 
 	 var a={};
 	 a.code="XXX";
 	 a.arr="something else";
 	 a.arr=[];  a.arr[0]={}; 	a.arr[0].key="something";
-	 $scope.isarray="default nothing happened";
+	 
+	 var arr=[2,3,5,7,9,11,12,5];
+	 
+	 var ret=filterFilter(arr,5,true);
+	 $log.debug("ret:"); 
+	 $log.debug(ret);
+	 
+	 var filter=function(elem) {
+		if (elem % 2 && elem !=11)
+			return true;
+		return false;
+	 };
+	 
+	 var ret2=filterFilter(arr,filter,true);
+	 $log.debug("ret:"); 
+	 $log.debug(ret2);
+	 
+	 
+	 $scope.isarray="default nothing happened";	 
+	 
 	 if (a.arr && angular.isArray(a.arr) && a.arr.length && a.arr[0] && a.arr[0].key) {
 		 $scope.isarray="array is there with key value: " + a.arr[0].key;
 	 }
